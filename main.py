077315856f709eb5053a9d69f767b174371f3eb4
nikita-efimov <nikikot@list.ru>
2019-08-06 14:53:58 +0300

    added print flag

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ def get_names() -> List[str]:
     return ['god', 'god2131231']
 
 
-def check_name(name: str) -> bool:
+def check_name(name: str, print_is_busy: bool = False):
     profile_link: str = GITHUB_LINK + name
     req = get(profile_link)
     status_code: int = req.status_code
-    if status_code == 200:
+    if status_code == 200 and print_is_busy:
         print(colored('Name is busy: ' + name, 'red'))
     elif status_code == 404:
         print(colored('Name is free: ' + name, 'green'))
-        
+
 
 def check_names(names_list: List[str]):
     th_list: List[Thread] = list()
